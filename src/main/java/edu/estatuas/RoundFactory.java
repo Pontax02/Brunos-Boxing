79ca99958b34b1dd64c335c0b4393e805b222013
Pontax02 <ppontanilla07@gmail.com>
2025-03-16 18:43:40 +0100

    :sparkles: build(RoundFactory): Recuperar metodo RoundFactory

--- src/main/java/edu/estatuas/RoundFactory.java
+++ src/main/java/edu/estatuas/RoundFactory.java
@@ -1,4 +1,4 @@
-/*package edu.estatuas;
+package edu.estatuas;
 
 public class RoundFactory {
 
@@ -8,13 +8,12 @@ public class RoundFactory {
     public static Round getRound(String roundScore){
 
         if (roundScore.equals("10-9")||roundScore.equals("9-10")){
-            regularRound newround = new regularRound
+            return new RegularRound(roundScore);
 
         }
-
+        return null;
 
 
     }
 
 }
-*/
