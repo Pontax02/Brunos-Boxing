c4eff8c68e61e009e8e4b2bf337d91b4eebc4c65
Pontax02 <ppontanilla07@gmail.com>
2025-03-20 20:16:08 +0100

    :sparkles: feat(RoundFactory): Añadir lógica a roundfactory

--- src/main/java/edu/estatuas/RoundFactory.java
+++ src/main/java/edu/estatuas/RoundFactory.java
@@ -9,9 +9,16 @@ public class RoundFactory {
         if (roundScore == null) {
             return null;
         }
-        if (roundScore.equalsIgnoreCase("10-9")||roundScore.equalsIgnoreCase("9-10")){
+        if (roundScore.equals("10-9")||roundScore.equals("9-10")){
             return new RegularRound(roundScore);
-
+        }
+        else if (roundScore.equals("10 - 8")
+                || roundScore.equals("8 - 10")) {
+            return new KnockdownRound(roundScore);
+        }
+        else if (roundScore.equalsIgnoreCase("1, 8 - 10")
+                || roundScore.equalsIgnoreCase("10 - 8 ,1")) {
+            return new PointsDeducted(roundScore);
         }
         return null;
 
