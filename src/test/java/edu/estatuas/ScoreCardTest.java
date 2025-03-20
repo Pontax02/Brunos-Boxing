44a8b710b00e4ed9290a8cae6ef7ebb79c4759d3
Pontax02 <ppontanilla07@gmail.com>
2025-03-20 19:49:02 +0100

    :lock: test(ScorecardTest): Añador casos test

--- src/test/java/edu/estatuas/ScoreCardTest.java
+++ src/test/java/edu/estatuas/ScoreCardTest.java
@@ -47,4 +47,43 @@ public class ScoreCardTest {
 
 
     }
+    @Test
+    public void loadJudgeScoreCardRoundFactoryRegularTest() {
+
+        card.loadJudgeScoreCard(whiteScoreCard);
+        assertEquals(10, card.getNumRounds());
+        assertTrue(card.getRounds().stream().allMatch(a -> a instanceof RegularRound));
+    }
+
+    @Test
+    public void loadJudgeScoreCardRoundFactoryNullTest() {
+        card.loadJudgeScoreCard(new String[]{null, null});
+        assertEquals(0, card.getNumRounds());
+    }
+
+    @Test
+    public void loadJudgeScoreCardRoundFactoryPointsDeductedTest() {
+
+        card.loadJudgeScoreCard(pinkScoreCard);
+        assertEquals(10, card.getNumRounds());
+        assertEquals(2, card.getRounds().stream().filter(a -> a instanceof PointsDeducted).count());
+    }
+
+
+    @Test
+    public void loadJudgeScoreCardRoundFactoryKnockDownTest() {
+
+        card.loadJudgeScoreCard(pinkScoreCard);
+        assertEquals(10, card.getNumRounds());
+        assertEquals(2, card.getRounds().stream().filter(a -> a instanceof KnockdownRound).count());
+    }
+
+    @Test
+    public void getBoxerFinalScoreTest() {
+        assertEquals(0, card.getRedBoxerFinalScore());
+        assertEquals(0, card.getBlueBoxerFinalScore());
+        card.loadJudgeScoreCard(whiteScoreCard);
+        assertEquals(94, card.getRedBoxerFinalScore());
+        assertEquals(96, card.getBlueBoxerFinalScore());
+    }
 }
