ed51e8ce10a136c32235b5712a2852f62af744da
Pontax02 <ppontanilla07@gmail.com>
2025-03-20 19:47:18 +0100

    :white_check_mark: test(ScorecardTest): Añadir cards a los casos test

--- src/test/java/edu/estatuas/ScoreCardTest.java
+++ src/test/java/edu/estatuas/ScoreCardTest.java
@@ -7,10 +7,31 @@ import org.junit.BeforeClass;
 public class ScoreCardTest {
 
     private static ScoreCard sc;
+    private String[] whiteScoreCard =  {"9 - 10",
+            "9 - 10",
+            "9 - 10",
+            "9 - 10",
+            "10 - 9",
+            "10 - 9",
+            "10 - 9",
+            "9 - 10",
+            "10 - 9",
+            "9 - 10"};
+
+    private String[] pinkScoreCard = {"9 - 10",
+            "9 - 10",
+            "9 - 10",
+            "1, 8 - 10",
+            "10 - 8",
+            "10 - 8 ,1",
+            "10 - 9",
+            "9 - 10",
+            "10 - 9",
+            "10 - 8"};
 
     @BeforeClass
     public static void setup() {
-        sc = new ScoreCard("white","Rocky Balboa","Apollo Creed");
+        sc = new ScoreCard("white");
     }
 
     @Test
